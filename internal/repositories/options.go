e43d9a3d711cd6f002032494bab8260400607bda
duyledat197 <duyledat197>
2023-03-02 01:20:51 +0700

    adding repo options

---
+++ internal/repositories/options.go
@@ -0,0 +1,14 @@
+package repositories
+
+import (
+	"github.com/jackc/pgx/v5"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Options struct {
+	// transaction for DBMS
+	Tx *pgx.Tx
+
+	// transaction for mongodb
+	SessionContext mongo.SessionContext
+}
